cd16c6dc3245c13f7b75215d65242ffc881cde61
Juri Kostjunin <juri.kostjunin@gmail.com>
2024-03-11 18:18:11 +0200

    feat: added import aliases

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components/index',
+      '@hooks': '/src/hooks/index',
+      '@types': '/src/types/index',
+      '@utils': '/src/utils/index',
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './vitest.setup.ts',
     coverage: {
       ...configDefaults.coverage,
-      exclude: [...(configDefaults.coverage.exclude ?? []), 'src/main.tsx'],
+      exclude: [...(configDefaults.coverage.exclude ?? []), 'src/main.tsx', 'src/components/index.ts'],
     },
   },
 });
